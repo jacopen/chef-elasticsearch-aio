58725f2ea50e53c18ec97ec9aa145a4966f5cad3
Kazuto Kusama <jacopen@gmail.com>
2013-11-18 22:27:47 +0900

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -2,9 +2,9 @@ name             'es_with_fluentd'
 maintainer       'Kazuto Kusama'
 maintainer_email 'jacopen@gmail.com'
 license          'All rights reserved'
-description      'Installs/Configures es_with_fluentd'
+description      'Installs/Configures Elasticsearch, Fluentd(td-agent), Kibana and Elasticsearch Head'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 depends "apt"
 depends "td-agent"
