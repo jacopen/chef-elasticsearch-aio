04d8d1718928bd688bb4d8680e35e78e7f770a7e
Kazuto Kusama <jacopen@gmail.com>
2014-08-14 14:40:01 +0900

    Add kopf and marvel

--- recipes/elasticsearch.rb
+++ recipes/elasticsearch.rb
@@ -20,31 +20,52 @@ template "/etc/elasticsearch/elasticsearch.yml" do
   notifies :restart, "service[elasticsearch]"
 end
 
+# Uninstall old plugins
 bash "Uninstall elasticsearch-head" do
   code "/usr/share/elasticsearch/bin/plugin -remove mobz/elasticsearch-head"
   user "root"
-  group "root"
   only_if "/usr/share/elasticsearch/bin/plugin --list | grep head"
 end
 
 bash "Uninstall Kibana" do
   code "/usr/share/elasticsearch/bin/plugin -remove elasticsearch/kibana3"
   user "root"
-  group "root"
   only_if "/usr/share/elasticsearch/bin/plugin --list | grep kibana"
 end
 
+bash "Uninstall kopf" do
+  code "/usr/share/elasticsearch/bin/plugin -remove lmenezes/elasticsearch-kopf"
+  user "root"
+  only_if "/usr/share/elasticsearch/bin/plugin --list | grep kopf"
+end
+
+bash "Uninstall marvel" do
+  code "/usr/share/elasticsearch/bin/plugin -remove elasticsearch/marvel/latest"
+  user "root"
+  only_if "/usr/share/elasticsearch/bin/plugin --list | grep marvel"
+end
+
+# Install plugins
 bash "Install elasticsearch-head" do
   code "/usr/share/elasticsearch/bin/plugin -install mobz/elasticsearch-head"
   user "root"
-  group "root"
 end
 
 bash "Install Kibana" do
   code "/usr/share/elasticsearch/bin/plugin -url http://download.elasticsearch.org/kibana/kibana/kibana-latest.zip -install elasticsearch/kibana3"
   user "root"
-  group "root"
 end
+
+bash "Install kopf" do
+  code "/usr/share/elasticsearch/bin/plugin -install lmenezes/elasticsearch-kopf"
+  user "root"
+end
+
+bash "Install marvel" do
+  code "/usr/share/elasticsearch/bin/plugin -install elasticsearch/marvel/latest"
+  user "root"
+end
+
 
 service "elasticsearch" do
   supports [:restart]
